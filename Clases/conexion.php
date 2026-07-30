0afa9147b2356be4bf14b2206aae42235472de92
kleiman_2009 <kleiman20092009@gmail.com>
2026-07-30 17:10:40 -0500

    conexion DB

--- Clases/conexion.php
+++ Clases/conexion.php
@@ -3,10 +3,10 @@ class Cconexion {
     // Agregamos "static" para poder llamarla con los dos puntos ::
     public static function ConexionBD() {
         
-        $host = "127.0.0.1:3306:";
-        $dbname = "repuestos";
-        $username = "root";
-        $password = "FamiliaMalagonVelez";
+        $host = "sql201.infinityfree.com";
+        $dbname = "if0_42532505_almacen";
+        $username = "if0_42532505";
+        $password = "ZdMTewqn7S3KS6";
 
         try {
             $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
